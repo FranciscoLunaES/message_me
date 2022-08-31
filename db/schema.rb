80fb2a3c215912d016a1aef9d6f116b7ff38398d
FranciscoLunaES <francisco.luna@enroutesystems.com>
2022-08-31 13:49:08 -0700

    Create messages resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_31_203136) do
+ActiveRecord::Schema.define(version: 2022_08_31_203957) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
